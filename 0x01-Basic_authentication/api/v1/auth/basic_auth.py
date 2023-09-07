16f96caadc5d3b06ca69351b5000ad19b3f04402
jaredatandi <jaredatandi07@gmail.com>
2023-09-08 01:06:03 +0300

    Extract base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,8 +6,25 @@ from api.v1.auth.auth import Auth
 from models.user import User
 from typing import Tuple, TypeVar
 import base64
+import re
+import binascii
+
+from .auth import Auth
+from models.user import User
 
 
 class BasicAuth(Auth):
     """Perform basic auth"""
-    pass
+
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        if authorization_header is None:
+            return None
+
+        if type(authorization_header) == str:
+            pattern = r'Basic (?<token>.+)'
+            match_pattern = re.fullmatch(pattern, authorization_header.strip())
+            if match_pattern is not None:
+                return match_pattern.group('token')
+        return None
