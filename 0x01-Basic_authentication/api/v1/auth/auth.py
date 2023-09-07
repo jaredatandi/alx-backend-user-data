964f5a7198f309892799b1392354f18908ed01b9
jaredatandi <jaredatandi07@gmail.com>
2023-09-07 22:47:26 +0300

    Implement require auth method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,6 +5,7 @@
 
 from flask import request
 from typing import List, TypeVar
+import fnmatch
 
 
 class Auth:
@@ -20,6 +21,16 @@ class Auth:
         Returns:
             bool: _description_
         """
+        if path is None:
+            return True
+
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        for excluded_path in excluded_paths:
+            if fnmatch.fnmatch(path, excluded_path):
+                return False
+
         return False
 
     def authorization_header(self, request=None) -> str:
