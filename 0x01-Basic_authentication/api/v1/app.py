ef6f3cd792e200155c9c6c79b5f2e2e01fe30118
jaredatandi <jaredatandi07@gmail.com>
2023-09-08 00:29:58 +0300

    Update the require auth list

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -55,6 +55,7 @@ def authenticate_user():
     """
     if auth:
         excluded_paths = [
+            '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden',
         ]
