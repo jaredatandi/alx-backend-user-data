18f1e376ec3778b502916ee007566802c7e0566d
jaredatandi <jaredatandi07@gmail.com>
2023-09-07 22:40:37 +0300

    Implement auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,38 @@
+"""Authentication module
+"""
+
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Authentication class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """routes that require auth
+
+        Args:
+            path (str): _description_
+            excluded_paths (List[str]): _description_
+
+        Returns:
+            bool: _description_
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Header data
+
+        Args:
+            request (_type_, optional): _description_. Defaults to None.
+
+        Returns:
+            str: _description_
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Determine user
+        """
+        return None
